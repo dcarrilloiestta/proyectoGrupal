0130bd1d84d46f7604ecc9411b1ede4beaa326b3
dcarrilloiestta <pruebapruebadsiii@gmail.com>
2021-11-10 08:43:39 -0500

    primera Carga

---
+++ resources/views/formPrincipal.blade.php
@@ -0,0 +1,4 @@
+@extends('plantilla')
+@section('campo2')
+    SOY UN TEXTO DENTRO DEL CAMPO 2 
+@endsection
---
+++ resources/views/plantilla.blade.php
@@ -0,0 +1,7 @@
+<!-- TÓDO EL CÓDIGO GENÉRICO -->
+    <BR> ESTE ES UN TÍTULO <BR>
+        <BR> MENÚ <BR>
+        @yield('campo1')
+    <section> SECCIÓN PRINCIPAL </section>
+    <BR> ESTA ES LA PARTE INFERIOR <BR>
+        @yield('campo2')
