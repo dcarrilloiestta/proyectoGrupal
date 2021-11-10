b142b7d1087ba4baf16457ad852e090fb0348e80
dcarrilloiestta <pruebapruebadsiii@gmail.com>
2021-11-10 09:12:07 -0500

    mejora plantilla

--- resources/views/plantilla.blade.php
+++ resources/views/plantilla.blade.php
@@ -1,6 +1,6 @@
 <!-- TÓDO EL CÓDIGO GENÉRICO -->
-    <BR> ESTE ES UN TÍTULO <BR>
-        <BR> MENÚ <BR>
+MENU 
+<div>MODIFICACIÓN </div>
         @yield('campo1')
     <section> SECCIÓN PRINCIPAL </section>
     <BR> ESTA ES LA PARTE INFERIOR <BR>
